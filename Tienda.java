6d70e8de116662806456af168f9893cc0719aab1
gtalejandro <skalexELJ@gmail.com>
2016-04-17 17:08:03 -0500

    Update Tienda.java

--- Tienda.java
+++ Tienda.java
@@ -9,113 +9,75 @@ public class Tienda {
 	static Producto p4 = new Producto("Chetos", 6.00);
 	static Producto p5 = new Producto("Cahuasakis", 32.50);
 	public static void main(String[] args) {
-		 System.out.println("\n\t\tBIENVENIDO A LA MINIMICROTIENDITITITA, ESTAMOS PARA SERVIRTE.");
+	 System.out.println("\n\t\tBIENVENIDO A LA MINIMICROTIENDITITITA, ESTAMOS PARA SERVIRTE.");
 		 System.out.println("\t\tEstos son los productos que tenemos a la venta: \n");
-		 System.out.println(p1);
-		 System.out.println(p2);
-		 System.out.println(p3);
-		 System.out.println(p4);
-		 System.out.println(p5);
-		 int res;
-		 int cantidad1 = 0;
-		 int cantidad2 = 0;
-		 int cantidad3 = 0;
-		 int cantidad4 = 0;
-		 int cantidad5 = 0;
+		 System.out.println("\t1.-" + p1);
+		 System.out.println("\t2.-" + p2);
+		 System.out.println("\t3.-" + p3);
+		 System.out.println("\t4.-" + p4);
+		 System.out.println("\t5.-" + p5);
+		 String r;
+		 double total = 0;
+		 double[] subTot = new double[5];
+		 double cantidad = 0;
+		 double pago =0;
+		 double cambio = 0;
 		 do {
 		 	int opc = 0;
-		 	System.out.println("\n\tQue producto deseas comprar?: ");
-		 	System.out.println("\n1: leche\n2: Refresco\n3: Condones\n4: Chetos\n5: Cahuasakis");
+		 	System.out.print("\n\tQue deseas producto deseas comprar?: ");
 		 	opc = input.nextInt();
 		 	switch(opc) {
 		 		case 1:
-		 		System.out.println("Cuantos litros de leche deseas comprar? ");
-		 		cantidad1 = input.nextInt();
-		 		p1.comprar(cantidad1);
+		 		System.out.print("\tCuantos litros de leche deseas comprar? ");
+		 		cantidad = input.nextDouble();
+		 		subTot[0] = p1.comprar(cantidad)*p1.getPrecio();
 		 		break;
 		 		case 2:
-		 		System.out.println("Cuantos litros de Refresco deseas comprar? ");
-		 		cantidad2 = input.nextInt();
-		 		p2.comprar(cantidad2);
+		 		System.out.print("\tCuantos litros de Refresco deseas comprar? ");
+		 		cantidad = input.nextDouble();
+		 		subTot[1] = p2.comprar(cantidad)*p2.getPrecio();
 		 		break;
 		 		case 3:
-		 		System.out.println("Cuantos Condones deseas comprar? ");
-		 		cantidad3 = input.nextInt();
-		 		p3.comprar(cantidad3);
+		 		System.out.print("\tCuantos Condones deseas comprar? ");
+		 		cantidad = input.nextDouble();
+		 		subTot[2] = p3.comprar(cantidad)*p3.getPrecio();
 		 		break;
 		 		case 4:
-		 		System.out.println("Cuantos Chetos deseas comprar? ");
-		 		cantidad4 = input.nextInt();
-		 		p4.comprar(cantidad4);
+		 		System.out.print("\tCuantos Chetos deseas comprar? ");
+		 		cantidad = input.nextDouble();
+		 		subTot[3] = p4.comprar(cantidad)*p4.getPrecio();
 		 		break;
 		 		case 5:
-		 		System.out.println("Cuantas cahuamas deseas comprar? ");
-		 		cantidad5 = input.nextInt();
-		 		p5.comprar(cantidad5);
+		 		System.out.print("\tCuantas cahuamas deseas comprar? ");
+		 		cantidad = input.nextDouble();
+		 		subTot[4] = p5.comprar(cantidad)*p5.getPrecio();
 		 		break;
 		 		default:
-		 		System.out.println("OPCION INCORRECTA");
+		 		System.out.println("\tOPCION INCORRECTA");
 		 	}
-		 	System.out.println("Deseas comprar otro? 1.si\t2.no)");
-		 	res = input.nextInt();
-		 	/*System.out.println(cantidad1);
-		 	System.out.println(cantidad2);
-		 	System.out.println(cantidad3);		Esta parte fue para ver que guardara las cantidades
-		 	System.out.println(cantidad4);
-		 	System.out.println(cantidad5);*/
-		 }while(res == 1);
-		 	
+		 	System.out.print("\tDeseas comprar otro?(s/n): ");
+		 	r = input.nextLine();
+		 	r = input.nextLine();
+		 }while(!r.equals("n"));
 		 
-		 //CALCULO DEL TOTAL
-		 double tot1 = 0;
-		 double tot2 = 0;
-		 double tot3 = 0;
-		 double tot4 = 0;
-		 double tot5 = 0;
-		 double total = 0;
-		 double pago = 0;
-		 double cambio = 0;
-		 double falta = 0;
-		 double completar = 0;
-		 //este calculo no trabaja
-		 tot1 = cantidad1*p1.precio;
-		 tot2 = cantidad1*p2.precio;
-		 tot3 = cantidad1*p3.precio;
-		 tot4 = cantidad1*p4.precio;
-		 tot5 = cantidad1*p5.precio;//hasta aqui, marca errores en el . del pn.precio para n del 1 al 5
-		 total = tot1 + tot2 + tot3 + tot4 + tot5;
-		 cambio = pago - total;
-		 System.out.println("Con cuanto dinero va a pagar?");
-		 pago = input.nextInt();
-		 do{
-		 	falta = total - cambio;
-		 	System.out.println("Te faltan $ " + falta + " pesos carnal\nCuanto mas dinero vas a dar?");
-		 	completar = input.nextInt();
-		 	pago = pago + completar;
-		 }while(pago < total);//aqui hay que hacer que vaya sumando lo que le falte de dinero
+		 for(double producto : subTot){
+			total+=producto;
+		}
+		System.out.println("\n\tSu total es de " + total);
+		System.out.print("\n\tEl cliente pago: ");
+		pago = input.nextDouble();
+		cambio = pago - total;
 
-		 //RECIBO
+		 //<RECIBO></RECIBO>
 		 System.out.println("\n*********************************");
 		 System.out.println("\n     TIENDA UNIVERSITARIA FI     ");
-		 System.out.println("\n\n*inserte aqui direccion");
+		 System.out.println("\n\n*Lomas-ajeas col del diablo #666");
 		 System.out.println("\n");
-		 System.out.println("\n");
-		 System.out.println("\n");
-		 if(cantidad1>0){
-		 	 System.out.println("\n" + cantidad1 + "\tLeche\t$ " + tot1);
-		 }
-		 if(cantidad2>0){
-		 	 System.out.println("\n" + cantidad2 + "\tRefresco\t$ " +  tot2);
-		 }
-		 if(cantidad3>0){
-		 	 System.out.println("\n" + cantidad3 + "\tCondones\t$ " +  tot3);
-		 }
-		 if(cantidad4>0){
-		 	 System.out.println("\n" + cantidad4 + "\tChetos\t$ " +  tot4);
-		 }
-		 if(cantidad5>0){
-		 	 System.out.println("\n" + cantidad5 + "\tCaguamas\t$ " +  tot5);
-		 }
+		 System.out.println("\n" + p1.getCompra() + "\tLeche\t$ " + subTot[0]);
+		 System.out.println("\n" + p2.getCompra() + "\tRefresco\t$ " +  subTot[1]);
+		 System.out.println("\n" + p3.getCompra() + "\tCondones\t$ " +  subTot[2]);
+		 System.out.println("\n" + p4.getCompra() + "\tChetos\t$ " +  subTot[3]);
+		 System.out.println("\n" + p5.getCompra() + "\tCaguamas\t$ " +  subTot[4]);
 		 System.out.println("\nTotal:\t$ " +  total);
 		 System.out.println("\n---------------------------------");
 		 System.out.println("\nPago de:\t $ " +  pago);
@@ -123,6 +85,6 @@ public class Tienda {
 		 System.out.println("\n---------------------------------");
 		 System.out.println("\n\tGRACIAS POR SU COMPRA\t");
 		 System.out.println("\n\n*********************************");
-
+	
 	}
 }
