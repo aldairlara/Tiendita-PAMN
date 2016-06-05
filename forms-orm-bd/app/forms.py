36bc74d38f28ee8deb2c75dee3b209b437529ce0
MauricioUrb <maurbina96@hotmail.com>
2016-06-04 23:53:49 -0500

    Add files via upload

---
+++ forms-orm-bd/app/forms.py
@@ -0,0 +1,21 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, IntegerField, HiddenField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+	login = StringField('login', validators=[DataRequired("Es requerido el login")])
+	password = PasswordField('password', validators=[DataRequired("Es requerido el password")])
+	remember_me = BooleanField('recuerdame', default=False)
+
+class RegistroForm(Form):
+	login = StringField('login', validators=[DataRequired("Es requerido el login")])
+	password = PasswordField('password', validators=[DataRequired("Es requerido el password")])
+	email = StringField('correo electronico', validators=[DataRequired("Es requerido el correo electronico")])
+
+class AgregarForm(Form):
+	cantidad = IntegerField("Cantidad", validators=[DataRequired("Se requiere la cantidad")])
+
+class EditForm(Form):
+	login = StringField('New user', validators=[DataRequired("Es requerido el login")])
+	password = PasswordField('New password', validators=[DataRequired("Es requerido el password")])
+	email = StringField('New correo electronico', validators=[DataRequired("Es requerido el correo electronico")])	
